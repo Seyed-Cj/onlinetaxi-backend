a8df768913aaf1edf6a99669c57036f575531de7
SeyedCj <seyedxcj@gmail.com>
2026-07-21 09:16:53 +0330

    test: add WebSocket client examples

---
+++ examples/driver-client.js
@@ -0,0 +1,38 @@
+const { io } = require("socket.io-client");
+
+const DRIVER_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dCI6IkRSSVZFUiIsImFpZCI6IjNmY2QwNjVjLTFlMjItNDdjNS1iOTllLTlmNDE3NmZlZjA3MyIsInNpZCI6ImUwYTM3OTc3LWZjZGMtNDAzYi1iNmNmLWUzZjBlNTJlZDIxNCIsImFlYSI6MTc4NDYwOTkxNjY1MSwicmVhIjoxNzg1MjEzODE2NjUxLCJpYXQiOjE3ODQ2MDkwMTYsImV4cCI6MTc4NDYwOTkxNn0.oLFypI7M57LdgFVXfZIdB8J-fAuiAr2J8v01UYLpMz8";
+
+const socket = io("http://localhost:3000", {
+  auth: {
+    token: DRIVER_TOKEN,
+  },
+});
+
+socket.on("connect", () => {
+  console.log("connected");
+
+  setInterval(() => {
+    const lat = 948.5 + Math.random() * 0.01;
+    const lng = 1100.2 + Math.random() * 0.01;
+
+    socket.emit("driver:location", { lat, lng });
+    console.log("send location", { lat, lng });
+  }, 5000);
+});
+
+socket.on("trip:new", (trip) => {
+  console.log("new trip:", trip);
+});
+
+socket.on("trip:accepted", (trip) => {
+  console.log("trip accepted:", trip);
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnected");
+});
+
+socket.on("connect_error", (error) => {
+  console.log(error.message);
+});
---
+++ examples/passenger-client.js
@@ -0,0 +1,25 @@
+const { io } = require("socket.io-client");
+
+const PASSENGER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dCI6IlBBU1NFTkdFUiIsImFpZCI6IjE2Y2QyZGQyLWI0ZGYtNDI3NS04NmY4LTFiMzI5OGUwMzI4MiIsInNpZCI6IjQyNTI5MjlkLTI2NWMtNDg5Yy1iMTMxLTIxZjI3ZGJjN2NiNyIsImFlYSI6MTc4NDYxMTE1MzI5NCwicmVhIjoxNzg1MjE1MDUzMjk0LCJpYXQiOjE3ODQ2MTAyNTMsImV4cCI6MTc4NDYxMTE1M30.4gU-6m3rJYtKP3BMLhLyZTn4BaVcVAbN3t6z6cSn66Q";
+
+const socket = io("http://localhost:3000", {
+  auth: {
+    token: PASSENGER_TOKEN,
+  },
+});
+
+socket.on("connect", () => {
+  console.log("connected");
+});
+
+socket.on("trip:accepted", (trip) => {
+  console.log("trip accepted:", trip.driverId);
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnected");
+});
+
+socket.on("connect_error", (error) => {
+  console.log(error.message);
+});
